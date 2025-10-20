b175ee04d74f28d829773e4fc06d68def5a0f97b
Pawan Kumar <74715354+Pawankumar32@users.noreply.github.com>
2025-10-20 21:31:43 +0530

    utils code  -error handling using (try catch and Promises)

---
+++ src/index.js
@@ -0,0 +1,18 @@
+import dotenv from "dotenv"
+import connectDB from "./db/indexDB.js"
+import {app} from './app.js'
+
+
+dotenv.config({
+    path: './env'
+})
+connectDB()
+    .then(() => {
+        const PORT = process.env.PORT || 8001
+        app.listen(PORT, () => {
+            console.log(`Server is Running at Port ${PORT}`)
+        })
+    })
+    .catch((err) => {
+        console.log("MongoDB connection Failed", err)
+    })
